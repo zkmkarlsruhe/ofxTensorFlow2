c8c6734ac10fd074688e73acefd0fa42491818d1
danomatika <danomatika@gmail.com>
2021-01-19 02:00:16 +0100

    OF style update for example_effnet, moved jpg to bin/data

--- example_effnet/src/ofApp.cpp
+++ example_effnet/src/ofApp.cpp
@@ -3,29 +3,32 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofSetFrameRate(60);
+	ofSetVerticalSync(true);
+	ofSetWindowTitle("example_effnet");
 
-    // use TensorFlow ops through the cppflow wrappers
-    // load a jpeg picture cast it to float and add a dimension for batches
-    auto input = cppflow::decode_jpeg(cppflow::read_file(std::string("my_cat.jpg")));
-    input = cppflow::cast(input, TF_UINT8, TF_FLOAT);
-    input = cppflow::expand_dims(input, 0);
+	// use TensorFlow ops through the cppflow wrappers
+	// load a jpeg picture cast it to float and add a dimension for batches
+	auto input = cppflow::decode_jpeg(cppflow::read_file(ofToDataPath("my_cat.jpg")));
+	input = cppflow::cast(input, TF_UINT8, TF_FLOAT);
+	input = cppflow::expand_dims(input, 0);
 
-    // load and infer the model
-    cppflow::model model(ofToDataPath("model"));
-    auto output = model(input);
+	// load and infer the model
+	cppflow::model model(ofToDataPath("model"));
+	auto output = model(input);
 
-    // interpret the output
-    auto maxLabel = cppflow::arg_max(output, 1);
-    std::cout << "Maximum likelihood: " << maxLabel << std::endl;
+	// interpret the output
+	auto maxLabel = cppflow::arg_max(output, 1);
+	ofLog() << "Maximum likelihood: " << maxLabel;
 
-    // access each element via the internal vector
-    auto outputVector = output.get_data<float>();
-    
-    std::cout << "[281] tabby cat: " << outputVector[281] << std::endl;
-    std::cout << "[282] tiger cat: " << outputVector[282]  << std::endl;
-    std::cout << "[283] persian cat: " << outputVector[283]  << std::endl;
-    std::cout << "[284] siamese cat: " << outputVector[284]  << std::endl;
-    std::cout << "[285] egyptian cat: " << outputVector[285]  << std::endl;
+	// access each element via the internal vector
+	auto outputVector = output.get_data<float>();
+	
+	ofLog() << "[281] tabby cat: " << outputVector[281];
+	ofLog() << "[282] tiger cat: " << outputVector[282];
+	ofLog() << "[283] persian cat: " << outputVector[283];
+	ofLog() << "[284] siamese cat: " << outputVector[284];
+	ofLog() << "[285] egyptian cat: " << outputVector[285];
 }
 
 //--------------------------------------------------------------
