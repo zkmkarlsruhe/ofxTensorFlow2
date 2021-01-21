bd2ae9074e1d6cb8aec5bd3bfc6cb02c3ed3b60b
Paul Bethge <paul.bethge@zkm.de>
2021-01-21 02:31:19 +0100

    simple mem layout test

---
+++ test/src/testMemoryLayout.h
@@ -0,0 +1,22 @@
+#include "cppflow/cppflow.h"
+
+
+void testMemoryLayout() {
+
+    const int size = 16;
+    std::vector <int> flat {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+    for (int i=0; i < size; i++){
+        std::cout << flat[i] << " ";
+    }
+
+    cppflow::tensor tensor(flat, {2,2,2,2});
+
+    auto tensorVector = tensor.get_data<int>();
+
+    for (int i=0; i < size; i++){
+        std::cout << tensorVector[i] << " ";
+    }
+
+
+}
