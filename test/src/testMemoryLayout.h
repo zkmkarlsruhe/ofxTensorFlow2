64fd79bcb7b86ce11be4e08ec1e453cba6381fec
Paul Bethge <paul.bethge@zkm.de>
2021-01-22 21:04:50 +0100

    logging, still no sufficient test

--- test/src/testMemoryLayout.h
+++ test/src/testMemoryLayout.h
@@ -24,17 +24,13 @@ void testMemoryLayout() {
     const int size = 16;
     std::vector <int> flat {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
-    for (int i=0; i < size; i++){
-        std::cout << flat[i] << " ";
-    }
+	ofLog() << "testMemoryLayout:" << vectorToString (flat);
 
     cppflow::tensor tensor(flat, {2,2,2,2});
 
     auto tensorVector = tensor.get_data<int>();
 
-    for (int i=0; i < size; i++){
-        std::cout << tensorVector[i] << " ";
-    }
+	ofLog() << "testMemoryLayout:" << vectorToString (tensorVector);
 
 
 }
