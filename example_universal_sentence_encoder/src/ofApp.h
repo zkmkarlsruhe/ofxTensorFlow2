2b634b25effa5951e84680bcdc6633239e544451
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-06-16 01:07:38 +0200

    Add files via upload

---
+++ example_universal_sentence_encoder/src/ofApp.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxTensorFlow2.h"
+#include "srtparser.h"
+
+class ofApp : public ofBaseApp {
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		ofxTF2::Model model;
+		std::vector<std::pair<std::vector<double>, std::string>> tensor_sub;
+		std::vector<double> currentVector;
+		std::string currentString;
+		std::string show;
+};
