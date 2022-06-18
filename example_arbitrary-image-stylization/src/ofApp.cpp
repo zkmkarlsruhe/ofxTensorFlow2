6d756f2e162b39a3b8c488592705fa0dcc08c98d
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-06-18 19:37:44 +0200

    Add files via upload

--- example_arbitrary-image-stylization/src/ofApp.cpp
+++ example_arbitrary-image-stylization/src/ofApp.cpp
@@ -4,7 +4,7 @@
 void ofApp::setup() {
 	ofSetFrameRate(60);
 	ofSetVerticalSync(true);
-	ofSetWindowTitle("example_style_transfer");
+	ofSetWindowTitle(ofToString(ofGetFrameRate()));
 	videoPlayer.load("Frenzy.mp4");
 	videoPlayer.play();
 	
@@ -18,7 +18,7 @@ void ofApp::setup() {
 	model.setup({ {"serving_default_placeholder"} ,{"serving_default_placeholder_1"} }, { {"StatefulPartitionedCall"} });
 	input = cppflow::decode_jpeg(cppflow::read_file(std::string(ofToDataPath("wave.jpg"))));
 	input = cppflow::cast(input, TF_UINT8, TF_FLOAT);
-    input = input / 255.f;
+	input = cppflow::div(input, cppflow::tensor({ 255.f }));
 	input = cppflow::expand_dims(input, 0);
 }
 
@@ -26,13 +26,12 @@ void ofApp::setup() {
 void ofApp::update() {
 	videoPlayer.update();
 	if (videoPlayer.isFrameNew()) {
-		ofPixels& pixels = videoPlayer.getPixels();
-		auto input2 = ofxTF2::pixelsToTensor(pixels);
+		input2 = ofxTF2::pixelsToTensor(videoPlayer.getPixels());
 		input2 = cppflow::cast(input2, TF_UINT8, TF_FLOAT);
-		input2 = input2 / 255.f;
+		input2 = cppflow::div(input2, cppflow::tensor({ 255.f }));
 		input2 = cppflow::expand_dims(input2, 0);
 		std::vector<cppflow::tensor> vectorOfInputTensors = {
-		 input2, input
+			input2, input
 		};
 		auto output = model.runMultiModel(vectorOfInputTensors);
 		shape = ofxTF2::getTensorShape(output[0]);
