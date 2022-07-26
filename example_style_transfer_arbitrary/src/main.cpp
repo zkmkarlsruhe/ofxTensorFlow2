c208081a5e5103f906b066c26d92302aa20f3e88
paul <bethgeph@gmail.com>
2022-07-26 17:14:07 +0200

    change rendering dimensions

--- example_style_transfer_arbitrary/src/main.cpp
+++ example_style_transfer_arbitrary/src/main.cpp
@@ -4,7 +4,7 @@
 //========================================================================
 int main() {
 
-	ofSetupOpenGL(1280, 720, OF_WINDOW); // <-------- setup the GL context
+	ofSetupOpenGL(520, 400, OF_WINDOW); // <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
